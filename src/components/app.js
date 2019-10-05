1f3e06d64df351897f9b82ca54a16806c2bb3464
thomasmudge <alephcult@gmail.com>
2019-10-05 17:25:16 -0500

    Fixed `search` functionality

--- src/components/app.js
+++ src/components/app.js
@@ -20,12 +20,5 @@ angular.module('video-player')
   },
   templateUrl: 'src/templates/app.html'
 })
-// .controller('appCtrl', function ($scope){
-//   // declare the data that you want to pass down to the child components.
- 
-// })
-// .component('app', {
-//   controller: 'appCtrl', 
-//   templateUrl: 'src/templates/app.html',
-// })
+
 
--- src/components/search.js
+++ src/components/search.js
@@ -1,22 +1,4 @@
 angular.module('video-player')
-// .controller('searchCtrl', function($scope){
-//   this.newQuery = '';
-//   this.onClick = () => {
-//     this.result(this.newQuery);
-//   };
-// })
-// .directive('search', function(){
-//   return {
-//     scope: {
-//       result: '<'
-//     },
-  
-//   controllerAs: '$ctrl',
-//   bindToController: true,
-//   templateUrl: 'src/templates/search.html',
-//   controller: 'searchCtrl'
-//   };
-// });
   .component('search', {
     bindings: {
       onSearched: '<'
